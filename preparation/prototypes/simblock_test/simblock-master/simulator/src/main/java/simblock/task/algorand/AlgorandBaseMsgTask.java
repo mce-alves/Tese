d3148f35ff0cbdf25521e63becb4f908f316be11
Miguel Alves <42934491+mce-alves@users.noreply.github.com>
2021-04-12 18:46:42 +0100

    Message types for the Algorand protocol

---
+++ preparation/prototypes/simblock_test/simblock-master/simulator/src/main/java/simblock/task/algorand/AlgorandBaseMsgTask.java
@@ -0,0 +1,41 @@
+package simblock.task.algorand;
+
+import simblock.block.Block;
+import simblock.node.Node;
+import simblock.task.AbstractMessageTask;
+
+public class AlgorandBaseMsgTask extends AbstractMessageTask {
+
+    private AlgorandMsgType type;
+    private int round, period, step;
+    private Block block;
+
+    /**
+     * Instantiates a new Vote message task.
+     *
+     * @param from   sender node
+     * @param to     receiver node
+     * @param t      type of vote
+     * @param round  round vote refers to
+     * @param period period vote refers to
+     * @param step   step vote refers to
+     */
+    public AlgorandBaseMsgTask(Node from, Node to, AlgorandMsgType t, int round, int period, int step, Block block) {
+        super(from, to);
+        this.type = t;
+        this.period = period;
+        this.round = round;
+        this.step = step;
+        this.block = block;
+    }
+
+    public AlgorandMsgType getType() {  return type; }
+
+    public int getRound() { return round; }
+
+    public int getPeriod() { return period; }
+
+    public int getStep() { return step; }
+
+    public Block getBlock() { return this.block; }
+}
---
+++ preparation/prototypes/simblock_test/simblock-master/simulator/src/main/java/simblock/task/algorand/AlgorandMsgType.java
@@ -0,0 +1,11 @@
+package simblock.task.algorand;
+
+/**
+ * The type of the votes exchanged in the Algorand Agreement protocol
+ */
+public enum AlgorandMsgType {
+    PROPOSAL,
+    SOFTVOTE,
+    CERTVOTE,
+    NEXTVOTE
+}
